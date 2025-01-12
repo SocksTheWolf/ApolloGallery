b50bbbcfc4a438b383cfa0477ef4b2e5750c6f1f
Andrzej <37420882+Endriur24@users.noreply.github.com>
2025-01-12 16:51:01 +0100

    photoswipe init moved to another file

---
+++ public/static/js/initPhotoSwipe.js
@@ -0,0 +1,36 @@
+import PhotoSwipeLightbox from "/static/js/photoswipe-lightbox.esm.min.js";
+const lightbox = new PhotoSwipeLightbox({
+  gallery: "#gallery-pswp",
+  children: "a",
+  pswpModule: () => import("/static/js/photoswipe.esm.min.js"),
+});
+lightbox.on("uiRegister", function () {
+  lightbox.pswp.ui.registerElement({
+    name: "download-button",
+    order: 8,
+    isButton: true,
+    tagName: "a",
+
+    // SVG with outline
+    html: {
+      isCustomSVG: true,
+      inner:
+        '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
+      outlineID: "pswp__icn-download",
+    },
+
+    onInit: (el, pswp) => {
+      el.setAttribute("download", "");
+      el.setAttribute("target", "_blank");
+      el.setAttribute("rel", "noopener");
+
+      pswp.on("change", () => {
+        const specialUrl = pswp.currSlide.data.src;
+        const regex = /cdn-cgi\/image\/[^/]+\/(.*)/;
+        const match = specialUrl.match(regex);
+        el.href = match ? match[1] : specialUrl;
+      });
+    },
+  });
+});
+lightbox.init();
--- src/components/gallery/gallerySingle.jsx
+++ src/components/gallery/gallerySingle.jsx
@@ -54,45 +54,8 @@ export const Gallery = ({ gallery, images, c }) => (
         )}
       </div>
     </section>
-    {html`<script type="module">
-        import PhotoSwipeLightbox from "/static/js/photoswipe-lightbox.esm.min.js";
-        const lightbox = new PhotoSwipeLightbox({
-          gallery: "#gallery-pswp",
-          children: "a",
-          pswpModule: () => import("/static/js/photoswipe.esm.min.js"),
-        });
-        lightbox.on("uiRegister", function () {
-          lightbox.pswp.ui.registerElement({
-            name: "download-button",
-            order: 8,
-            isButton: true,
-            tagName: "a",
-
-            // SVG with outline
-            html: {
-              isCustomSVG: true,
-              inner:
-                '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
-              outlineID: "pswp__icn-download",
-            },
-
-            onInit: (el, pswp) => {
-              el.setAttribute("download", "");
-              el.setAttribute("target", "_blank");
-              el.setAttribute("rel", "noopener");
-
-              pswp.on("change", () => {
-                const specialUrl = pswp.currSlide.data.src;
-                const regex = /cdn-cgi\/image\/[^/]+\/(.*)/;
-                const match = specialUrl.match(regex);
-                el.href = match ? match[1] : specialUrl;
-              });
-            },
-          });
-        });
-        lightbox.init();
-      </script>
-
+    {html`
+      <script type="module" src="/static/js/initPhotoSwipe.js"></script>
       <script src="https://npmcdn.com/imagesloaded@4.1.4/imagesloaded.pkgd.js"></script>
       <script src="https://npmcdn.com/isotope-layout@3.0.6/dist/isotope.pkgd.js"></script>
       <script src="/static/js/mansory.js"></script>`}
