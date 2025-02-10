cdb38cd709698b31e3172b7140c88e6767c64446
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2025-02-09 19:02:57 -0800

    Use pico, update layout slightly
    Adds theme toggles as well

--- src/components/gallery/layout.jsx
+++ src/components/gallery/layout.jsx
@@ -1,28 +1,41 @@
 import { html } from 'hono/html'
 import { SocialMetaTags } from "../utils/metaTags"
 
-
 export const Layout = (props) => {
   const c = props.c;
   const desc = props.desc || c.env.DESCRIPTION;
   return (
     html`<!doctype html>
-    <html lang=${c.t()}>
+    <html data-theme="auto" lang="${c.t()}">
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
         <title>${props.title}</title>
         ${<SocialMetaTags title={props.title} desc={desc} url={c.req.url} />}
         <link rel="stylesheet" href="/static/style.css" />
+        <link rel="stylesheet" href="/static/gallery.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@yohns/picocss@2.2.10/css/pico.min.css" />
         <link rel="stylesheet" href="/static/photoswipe.css" />
       </head>
       <body>
-        <header>
-          <div class="container">
-            <h1>${c.env.PAGE_TITLE}</h1>
-          </div>
+        <header class="container">
+        <nav>
+          <ul>
+          <li><h2>${c.env.PAGE_TITLE}</h2></li>
+          </ul>
+          <ul>
+          <li>
+            <label data-tooltip="Toggle Light or Dark Mode">
+              <input name="color-mode-toggle" role="switch" type="checkbox" value="1" aria-label="Toggle Light or Dark Mode">
+            </label>
+          </li>
+          </ul>
+        </nav>
         </header>
-        <main>${props.children}</main>
+        <main class="container">
+          ${props.children}
+        </main>
+        <script src="/static/js/SwitchColorMode.js"></script>
       </body>
     </html>`
   );
