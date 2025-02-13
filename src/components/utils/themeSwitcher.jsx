5a7a47f0c45722fb290a3bf225db728596184859
Andrzej <37420882+Endriur24@users.noreply.github.com>
2025-02-14 00:36:02 +0100

    Fix too wide page on mobile
    tooltip positioned on bottom caused the issue

--- src/components/utils/themeSwitcher.jsx
+++ src/components/utils/themeSwitcher.jsx
@@ -1,7 +1,7 @@
 export const ThemeSwitcher = (props) => {
     const c = props.c;
     return (
-      <label data-placement="bottom" data-tooltip={c.t("light_or_dark_mode")}>
+      <label data-placement="left" data-tooltip={c.t("light_or_dark_mode")}>
         <input name="color-mode-toggle" role="switch" type="checkbox" value="1" />
         <script type="module" src="/static/js/SwitchColorMode.js"></script>
       </label>
