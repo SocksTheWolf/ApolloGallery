f0d994af4482c63c51374d2e2a4d0ccd57ac322d
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2025-02-15 15:36:35 -0800

    Handle 404 pages properly
    By feeding them to our static 404 page.

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,22 @@ const galleryApp = new Hono({ strict: true });
 
 galleryApp.route('/', gallery);
 staticFiles.use('/*', serveStatic({ root: './dist' }));
+staticFiles.use('/404', serveStatic({ path: './dist/404.html' }));
 galleryApp.route(`/*`, staticFiles);
+
+// Handle not found displaying a similar style of webpage
+galleryApp.notFound(async (c) => {
+    // Rebuild the url so we can fetch the 404 file specifically
+    const {origin} = new URL(c.req.url);
+    // Fetch it off our app, which will serve us back the 404 html file.
+    const get404 = await staticFiles.fetch(new Request(`${origin}/404`, c.env.req), c.env);
+    // Serve the result to the user.
+    if (get404) {
+        return c.html(await get404.text(), 404);
+    } else {
+        return c.text("404 Not Found", 404);
+    }
+});
 
 // Custom worker path system to handle dynamic pathing based off a path location variable
 // rather than hardcoded routes
