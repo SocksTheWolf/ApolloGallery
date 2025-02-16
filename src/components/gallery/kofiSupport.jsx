39fbd6c95252bc6246d4f1f79eb1f67331b4744f
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2025-02-16 12:56:07 -0800

    Update kofiSupport.jsx
    Allow for rendering if the Kofi script file is blocked

--- src/components/gallery/kofiSupport.jsx
+++ src/components/gallery/kofiSupport.jsx
@@ -10,16 +10,26 @@ export const KofiSupport = (props) => {
 
     // everything in the li is going to get injected with the kofi data upon load.
     // the original script will also be deleted, which is neat.
+    //
+    // some safari blockers will block the kofi script directly so have a fallback, 
+    // eventually copy the original script and host it locally
+    // to prevent problems
     return (
         <li id="kofi-btn" aria-busy="true" data-placement="bottom" data-tooltip={c.t("kofi_tooltip")}>
         <script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script>
         <script>
         {html`
-            kofiwidget2.init('${c.t("kofi_label")}', 'var(--pico-color)', '${kofiUsername}');
+            const kofiButton = document.getElementById("kofi-btn");
             window.addEventListener('load', function (){
-                const kofiButton = document.getElementById("kofi-btn");
                 kofiButton.removeAttribute("aria-busy");
-                kofiButton.innerHTML = kofiwidget2.getHTML();
+                 let replaceHTML = "";
+                 try {
+                     kofiwidget2.init('${c.t("kofi_label")}', 'var(--pico-color)', '${kofiUsername}');
+                     replaceHTML = kofiwidget2.getHTML();
+                 } catch {
+                     replaceHTML = "<a href='https://ko-fi.com/${kofiUsername}' target='_blank'><button>${c.t('kofi_label')}</button></a>";
+                 }
+                kofiButton.innerHTML = replaceHTML;
             });
         `}
         </script>
